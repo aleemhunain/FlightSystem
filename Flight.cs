7334346fa39d1f574e8601e3d07905a205b411e1
Hunain Aleem <aleemhunain@yahoo.ca>
2024-09-17 21:57:54 -0400

    Add project files.

---
+++ Flight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSystem
+{
+    internal class Flight
+    {
+        private string flightNumber;
+        private int availableSeats;
+
+        public string FlightNumber
+        {
+            get; set;
+        }
+
+        public int AvailableSeats
+        {
+            get; set;
+        }
+
+        public Flight(string flightNumber, int availableSeats)
+        {
+            FlightNumber = flightNumber;
+            AvailableSeats = availableSeats;
+        }
+
+        public void Booking(int numSeats, string customerName)
+        {
+            if (numSeats <= AvailableSeats)
+            {
+                AvailableSeats -= numSeats;
+                Console.WriteLine($"Booking successful for customer: {customerName} for flight {FlightNumber}");
+            } 
+            else
+            {
+                Console.WriteLine("Booking could not be completed due to insufficient seats");
+            }
+        }
+
+        public bool CheckFlightStatus(string queryFlightNumber, string customerName)
+        {
+            if (queryFlightNumber == FlightNumber)
+            {
+                Console.WriteLine($"Available seats: {AvailableSeats}, Thank you {customerName} for choosing our airline.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Flight number is incorrect");
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace FlightSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please provide a Flight Number: ");
+            string flightNumber = Console.ReadLine();
+
+            int availableSeats = 0; //added for error prevention
+            bool valSeat = false;
+
+            while (!valSeat)
+            {
+                try
+                {
+                    Console.Write("Please provide the Number of Available Seats: ");
+                    availableSeats = Convert.ToInt32(Console.ReadLine());
+                    valSeat = true;
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Please provide a valid number");
+                }
+            }
+
+
+            Flight flight1 = new Flight(flightNumber, availableSeats);
+
+            int desiredSeats = 0; //added for error prevention
+            bool valRequired = false;
+
+            while (!valRequired)
+            {
+                try
+                {
+                    Console.Write("Please provide the number of seats you wish to book: ");
+                    desiredSeats = Convert.ToInt32(Console.ReadLine());
+                    valRequired = true;
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Please provide a valid number");
+                }
+            }
+
+            Console.Write("Please provide your name: ");
+            string customerName = Console.ReadLine();
+
+            flight1.Booking(desiredSeats, customerName);
+
+            Console.Write("Please provide the Flight Number you'd like to check: ");
+            string flightInquire = flightInquire = Console.ReadLine(); ;
+            
+            flight1.CheckFlightStatus(flightInquire, customerName);
+
+        }
+    }
+}
